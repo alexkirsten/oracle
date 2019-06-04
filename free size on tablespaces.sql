7cca18956e046afd25f6df5436cec9b7439a7cb6
alexkirsten <49196649+alexkirsten@users.noreply.github.com>
2019-06-04 11:02:56 -0300

    Add files via upload

--- free size on tablespaces.sql
+++ free size on tablespaces.sql
@@ -1,4 +1,8 @@
-select df.tablespace_name, df.bytes bytes_free
+select df.tablespace_name,
+       df.bytes bytes_free,
+       trunc(df.bytes / 1024, 2) kbytes_free,
+       trunc(df.bytes / 1024 / 1024, 2) mbytes_free,
+       trunc(df.bytes / 1024 / 1024 / 1024, 2) gbytes_free
   from (select tablespace_name, sum(bytes) bytes
           from dba_data_files
          group by tablespace_name) df,
